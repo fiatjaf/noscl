c9d044448c67538ec75334fa75101303c599bc86
Honza Pokorny <honza@pokorny.ca>
2022-01-08 17:20:26 -0400

    Extract metadata into a struct

--- metadata.go
+++ metadata.go
@@ -9,18 +9,24 @@ import (
 	"github.com/fiatjaf/go-nostr"
 )
 
+type Metadata struct {
+	Name    string `json:"name,omitempty"`
+	About   string `json:"about,omitempty"`
+	Picture string `json:"picture,omitempty"`
+}
+
 func setMetadata(opts docopt.Opts) {
 	initNostr()
 
 	name, _ := opts.String("--name")
-	description, _ := opts.String("--description")
-	image, _ := opts.String("--image")
+	about, _ := opts.String("--about")
+	picture, _ := opts.String("--picture")
 
-	jmetadata, _ := json.Marshal(struct {
-		Name        string `json:"name,omitempty"`
-		Description string `json:"image,omitempty"`
-		Image       string `json:"image,omitempty"`
-	}{name, description, image})
+	jmetadata, _ := json.Marshal(Metadata{
+		Name:    name,
+		About:   about,
+		Picture: picture,
+	})
 
 	event, statuses, err := pool.PublishEvent(&nostr.Event{
 		PubKey:    getPubKey(config.PrivateKey),
