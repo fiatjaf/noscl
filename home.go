b3674539025360d3abe9b9beb404f210c97754d4
lgh <913337456@qq.com>
2023-03-17 09:40:46 +0800

    use nostr.KindEncryptedDirectMessage replace 4 in home.go and add .idea to gitignore file.

--- home.go
+++ home.go
@@ -3,8 +3,8 @@ package main
 import (
 	"encoding/json"
 	"log"
-    "strconv"
-    "time"
+	"strconv"
+	"time"
 
 	"github.com/docopt/docopt-go"
 	"github.com/nbd-wtf/go-nostr"
@@ -19,22 +19,22 @@ func home(opts docopt.Opts, inboxMode bool) {
 	initNostr()
 
 	verbose, _ := opts.Bool("--verbose")
-    jsonformat, _ := opts.Bool("--json")
-    noreplies, _ := opts.Bool("--noreplies")
-    onlyreplies, _ := opts.Bool("--onlyreplies")
-    kinds, kindserr := optSlice(opts, "--kinds")
-    if kindserr != nil {
-        return
-    }
-    var intkinds []int
-    for _, kind := range kinds {
-        if i, e := strconv.Atoi(kind); e == nil {
-            intkinds = append(intkinds, i)
-        }
-    }
-    since, _ := opts.Int("--since")
-    until, _ := opts.Int("--until")
-    limit, _ := opts.Int("--limit")
+	jsonformat, _ := opts.Bool("--json")
+	noreplies, _ := opts.Bool("--noreplies")
+	onlyreplies, _ := opts.Bool("--onlyreplies")
+	kinds, kindserr := optSlice(opts, "--kinds")
+	if kindserr != nil {
+		return
+	}
+	var intkinds []int
+	for _, kind := range kinds {
+		if i, e := strconv.Atoi(kind); e == nil {
+			intkinds = append(intkinds, i)
+		}
+	}
+	since, _ := opts.Int("--since")
+	until, _ := opts.Int("--until")
+	limit, _ := opts.Int("--limit")
 
 	var keys []string
 	nameMap := map[string]string{}
@@ -44,26 +44,26 @@ func home(opts docopt.Opts, inboxMode bool) {
 			nameMap[follow.Key] = follow.Name
 		}
 	}
-    pubkey := getPubKey(config.PrivateKey)
-    filters := nostr.Filters{{Limit: limit}}
-    if inboxMode {
-        // Filter by p tag to me
-        filters[0].Tags = nostr.TagMap{"p": {pubkey}}
-        // Force kinds to encrypted messages
-        intkinds = make([]int, 0)
-        intkinds = append(intkinds, 4)
-    } else {
-        filters[0].Authors = keys
-    }
-    if since > 0 {
-        sinceTime := time.Unix(int64(since), 0)
-        filters[0].Since = &sinceTime
-    }
-    if until > 0 {
-        untilTime := time.Unix(int64(until), 0)
-        filters[0].Until = &untilTime
-    }
-    filters[0].Kinds = intkinds
+	pubkey := getPubKey(config.PrivateKey)
+	filters := nostr.Filters{{Limit: limit}}
+	if inboxMode {
+		// Filter by p tag to me
+		filters[0].Tags = nostr.TagMap{"p": {pubkey}}
+		// Force kinds to encrypted messages
+		intkinds = make([]int, 0)
+		intkinds = append(intkinds, nostr.KindEncryptedDirectMessage)
+	} else {
+		filters[0].Authors = keys
+	}
+	if since > 0 {
+		sinceTime := time.Unix(int64(since), 0)
+		filters[0].Since = &sinceTime
+	}
+	if until > 0 {
+		untilTime := time.Unix(int64(until), 0)
+		filters[0].Until = &untilTime
+	}
+	filters[0].Kinds = intkinds
 	_, all := pool.Sub(filters)
 	for event := range nostr.Unique(all) {
 		// Do we have a nick for the author of this message?
@@ -87,21 +87,21 @@ func home(opts docopt.Opts, inboxMode bool) {
 			}
 		}
 
-        // if only want events referencing another
-        if (onlyreplies || noreplies) {
-            var hasReferences bool = false
-            for _, tag := range event.Tags {
-                if tag[0] == "e" {
-                    hasReferences = true
-                    if noreplies {
-                        continue
-                    }
-                }
-            }
-            if (onlyreplies && !hasReferences) {
-                continue
-            }
-        }
+		// if only want events referencing another
+		if onlyreplies || noreplies {
+			var hasReferences bool = false
+			for _, tag := range event.Tags {
+				if tag[0] == "e" {
+					hasReferences = true
+					if noreplies {
+						continue
+					}
+				}
+			}
+			if onlyreplies && !hasReferences {
+				continue
+			}
+		}
 
 		printEvent(event, &nick, verbose, jsonformat)
 	}
